c47c83ddb489c3c40787dcf87ffcf56063599681
te6000-16 <shashika8596@gmail.com>
2018-12-24 11:48:28 +0700

    First commit

---
+++ cmake-build-debug/CMakeFiles/opencv4_test.dir/DependInfo.cmake
@@ -0,0 +1,73 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/te6000-16/CLionProjects/opencv4_test/main.cpp" "/home/te6000-16/CLionProjects/opencv4_test/cmake-build-debug/CMakeFiles/opencv4_test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/te6000-16/opencv-4.0.0/build"
+  "/home/te6000-16/opencv-4.0.0/include"
+  "/home/te6000-16/opencv-4.0.0/modules/core/include"
+  "/home/te6000-16/opencv-4.0.0/modules/flann/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/hdf/include"
+  "/home/te6000-16/opencv-4.0.0/modules/imgproc/include"
+  "/home/te6000-16/opencv-4.0.0/modules/ml/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/phase_unwrapping/include"
+  "/home/te6000-16/opencv-4.0.0/modules/photo/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/plot/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/reg/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/surface_matching/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/viz/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/xphoto/include"
+  "/home/te6000-16/opencv-4.0.0/modules/dnn/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/freetype/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/fuzzy/include"
+  "/home/te6000-16/opencv-4.0.0/modules/gapi/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/hfs/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/img_hash/include"
+  "/home/te6000-16/opencv-4.0.0/modules/imgcodecs/include"
+  "/home/te6000-16/opencv-4.0.0/modules/videoio/include"
+  "/home/te6000-16/opencv-4.0.0/modules/highgui/include"
+  "/home/te6000-16/opencv-4.0.0/modules/ts/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/bioinspired/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/dnn_objdetect/include"
+  "/home/te6000-16/opencv-4.0.0/modules/features2d/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/line_descriptor/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/saliency/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/text/include"
+  "/home/te6000-16/opencv-4.0.0/modules/calib3d/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/ccalib/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/cvv/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/datasets/include"
+  "/home/te6000-16/opencv-4.0.0/modules/objdetect/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/rgbd/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/shape/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/stereo/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/structured_light/include"
+  "/home/te6000-16/opencv-4.0.0/modules/video/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/videostab/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/xfeatures2d/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/ximgproc/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/xobjdetect/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/aruco/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/bgsegm/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/dpm/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/face/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/optflow/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/sfm/include"
+  "/home/te6000-16/opencv-4.0.0/modules/stitching/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/superres/include"
+  "/home/te6000-16/opencv_contrib-4.0.0/modules/tracking/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/opencv4_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv4_test.dir/main.cpp.o"
+  "opencv4_test.pdb"
+  "opencv4_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv4_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
